3ec7114121bebbb3f1c84c7072639d3b3d3e6b6b
AlexTaber & ScottBWar <pair+alextaber.scottbwar@devbootcamp.com>
2015-05-25 11:47:44 -0400

    preview updates text inside textbox

--- app.js
+++ app.js
@@ -2,14 +2,38 @@
 var rawText = {}
 rawText.text = ""
 
+function formatHeader(text) {
+  if(text[0] == "#") {
+    headerArray = ["h1", "h2", "h3", "h4","h5","h6"];
+    index = text.match(/^#*/)[0].length - 1;
+    console.log(index)
+    text = text.replace(text.match(/^#*/)[0],"");
+    return "<" + headerArray[index] + ">" + text + "</" + headerArray[index] + ">";
+  }
+}
+
+function checkHeader(text) {
+  if(text[0] == "#") return true;
+  else return false;
+}
 //views
 function displayPreview() {
-  var textBox = document.getElementById("md_raw_text")
-  document.getElementById("preview_markdown").innerHTML = textBox.value
+  document.getElementById("preview_markdown").innerHTML = convertText()
 }
 
 //controller
-function convertText() {}
+function convertText() {
+  var textBox = document.getElementById("md_raw_text")
+  var rawText = textBox.value
+  var formattedText = ""
+  var rawTextArray = rawText.split('\n');
+  rawTextArray.forEach(function(text){
+    if(checkHeader(text)) formattedText += formatHeader(text);
+    else formattedText += text + "<br>"
+
+  });
+  return formattedText
+}
 
 //runner
 document.addEventListener("keyup", displayPreview);
